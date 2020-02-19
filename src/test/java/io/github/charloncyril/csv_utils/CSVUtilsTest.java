d9d3513de53cd9bfbe87b2d06b4958ab111d23fc
HenryJulienDesgranges <henry-julien.desgranges@orange.fr>
2020-02-19 23:58:29 +0100

    Update JUnit test and use getFile function for generate expected file instead of native object File

--- src/test/java/io/github/charloncyril/csv_utils/CSVUtilsTest.java
+++ src/test/java/io/github/charloncyril/csv_utils/CSVUtilsTest.java
@@ -1,17 +1,18 @@
 package io.github.charloncyril.csv_utils;
 
 import static org.junit.Assert.*;
-import java.io.File;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import io.github.charloncyril.file_utils.FileUtils;
 
 public class CSVUtilsTest {
 
 	private List<String[]> csvRows;
 
 	public CSVUtilsTest() {
-		csvRows = CSVUtils.readRowOrientedFile(new File("src/test/resources/info.csv"));
+		csvRows = CSVUtils.readRowOrientedFile(FileUtils.getFile("src/test/resources/","info.csv"));
 	}
 
 	@Test
@@ -21,7 +22,7 @@ public class CSVUtilsTest {
 
 	@Test
 	public void test_writeIntoCSVFile() {
-		Assertions.assertThatCode(() -> CSVUtils.writeIntoCSVFile(new File("src/test/resources/info.csv"), csvRows))
+		Assertions.assertThatCode(() -> CSVUtils.writeIntoCSVFile(FileUtils.getFile("src/test/resources/","info.csv"), csvRows))
 				.doesNotThrowAnyException();
 	}
 
--- src/test/java/io/github/charloncyril/json_utils/JSONUtilsTest.java
+++ src/test/java/io/github/charloncyril/json_utils/JSONUtilsTest.java
@@ -1,10 +1,10 @@
 package io.github.charloncyril.json_utils;
 
 import static org.junit.Assert.*;
-import java.io.File;
 import java.io.FileNotFoundException;
 import org.junit.Test;
 
+import io.github.charloncyril.file_utils.FileUtils;
 import io.github.charloncyril.models.AnonymizeColumn;
 import io.github.charloncyril.models.RulesColumn;
 import io.github.charloncyril.models.TypeColumn;
@@ -14,16 +14,16 @@ public class JSONUtilsTest {
 	@SuppressWarnings("unchecked")
 	@Test
 	public <T> void test_readJson() throws FileNotFoundException {
-		T[] array = (T[]) JSONUtils.readJson(new File("src/test/resources/csv_description.json"), TypeColumn[].class);
+		T[] array = (T[]) JSONUtils.readJson(FileUtils.getFile("src/test/resources/","csv_description.json"), TypeColumn[].class);
 		assertNotNull(array);
 		assertTrue(array instanceof TypeColumn[]);
 
-		array = (T[]) JSONUtils.readJson(new File("src/test/resources/csv_verification_rules.json"),
+		array = (T[]) JSONUtils.readJson(FileUtils.getFile("src/test/resources/","csv_verification_rules.json"),
 				RulesColumn[].class);
 		assertNotNull(array);
 		assertTrue(array instanceof RulesColumn[]);
 
-		array = (T[]) JSONUtils.readJson(new File("src/test/resources/csv_anonymization_rules.json"),
+		array = (T[]) JSONUtils.readJson(FileUtils.getFile("src/test/resources/","csv_anonymization_rules.json"),
 				AnonymizeColumn[].class);
 		assertNotNull(array);
 		assertTrue(array instanceof AnonymizeColumn[]);
