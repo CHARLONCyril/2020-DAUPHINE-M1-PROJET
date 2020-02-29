3bac038e5da4010f1c6ed748d31162f2a06a965a
CHARLONCyril <cyril0308@gmail.com>
2020-02-29 15:16:59 +0100

    implement system of log. Both results are writen into console with special colro depend on the level of the message and also in syslog file

---
+++ src/test/java/io/github/charloncyril/log/LoggerTest.java
@@ -0,0 +1,22 @@
+package io.github.charloncyril.log;
+
+import static org.junit.Assert.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import io.github.charloncyril.file_utils.FileUtils;
+
+public class LoggerTest {
+	
+	private static final Path RESSOURCES_FOLDER_TEST_LOG = Paths.get("src", "main", "resources","logs");
+
+	@Test
+	public void test_logContentParse() {
+		Logger.logContentParse(LoggerTest.class, "test logger function");
+		assertTrue(FileUtils.getFile(RESSOURCES_FOLDER_TEST_LOG.toString(), "/syslog").exists());
+	}
+
+}
